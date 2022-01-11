c0ffc057f95faebbcd526802ca04ff8f8f9424d2
Ross Allen <ro27711@mit.edu>
2022-01-11 11:46:19 -0800

    organizing data generation

---
+++ data/REAME.md
@@ -0,0 +1,1 @@
+A directory for locally storing generated data
--- src/pyrmm/datamakers/dubins.py
+++ src/pyrmm/datamakers/dubins.py
@@ -1,11 +1,14 @@
 import argparse
 import hashlib
 import git
+import yaml
+from collections import OrderedDict
 from datetime import datetime
 from pathlib import Path
 
 HASH_LEN = 5
 DESCRIPTOR = 'dubins'
+SAVE_DIR = 'data'
 
 # Setup argument parser
 parser = argparse.ArgumentParser()
@@ -19,46 +22,73 @@ parser.add_argument('--tree-branching', type=int, default=16, help='number of br
 parser.add_argument('--n-steps', type=int, default=2, help='number of intermediate steps in each control segment for collision checking')
 parser.add_argument('--n-samples', type=int, default=1024, help='number of top-level samples to draw for data generation')
 
-def get_file_prefix(inargs):
+def get_savefile(inargs):
     '''create the save filename using timestamps and hashes
     
     Returns:
-        fname : str
+        save_fname : str
             filename is formatted with the datetime when the file was called,
             the git hash of the repo
             string desriptor of the datatype
             the sha1 hash of this files contents (may change between repo commits)
             the sha1 hash of the input args
             YYYYMMDD_HHMMSS_DESC_REPO_FILE_ARGS
+        configs : dict
+            configurations to be saved into yaml file
     '''
+
+    configs = OrderedDict()
+    # configs = dict()
+
+    src_fpath = str(Path(__file__).resolve())
+    repo = git.Repo(search_parent_directories=True)
+    repo_dir = repo.git_dir[:-len('.git')]
+    # configs['src_file']= src_fpath.removeprefix(repo.git_dir.removesuffix('.git'))
+    configs['src_file']= src_fpath[len(repo_dir):]
 
     # get datetime for file naming
     dt_str = datetime.now().strftime('%Y%m%d_%H%M%S')
+    configs['datetime'] = dt_str
     # print(dt_str)
 
     # get git repo hash for file naming
-    repo = git.Repo(search_parent_directories=True)
     repo_hash = repo.head.object.hexsha
+    configs['git_hash'] = repo_hash
 
     # hash file contents
-    with open (Path(__file__).resolve(), 'r') as thisfile:
+    with open (src_fpath, 'r') as thisfile:
         file_str=thisfile.readlines()
     file_str = ''.join([i for i in file_str])
     file_hash = hashlib.sha1()
     file_hash.update(file_str.encode())
-    # print(file_hash.hexdigest()[:HASH_LEN])
+    configs['file_hash'] = file_hash.hexdigest()
     
     # hash input arguments
     args_hash = hashlib.sha1()
     args_hash.update(str(inargs).encode())
-    # print(args_hash.hexdigest()[:HASH_LEN])
+    configs['args_hash'] = args_hash.hexdigest()
 
-    return (
+    # add args to configs
+    # configs = {**configs, **vars(args)}
+    # configs.update(vars(args))
+
+    # create filename
+    save_fname = (
         dt_str + '_' +
         DESCRIPTOR + '_' +
         repo_hash[:HASH_LEN] + '_' + 
         file_hash.hexdigest()[:HASH_LEN] + '_' + 
         args_hash.hexdigest()[:HASH_LEN])
+
+    # save configs as yaml
+    config_fname = Path(repo_dir).resolve().joinpath(SAVE_DIR, save_fname+'.yml')
+    with open(config_fname, 'w') as config_file:
+        # yaml.dump({**configs}, config_file)
+        for k, v in configs.items():
+            yaml.dump({k:v}, config_file)
+        yaml.dump(vars(args), config_file)
+
+    return save_fname
 
 
 if __name__ == "__main__":
@@ -66,5 +96,5 @@ if __name__ == "__main__":
     # get input args
     args = parser.parse_args()
 
-    savefile_prfx = get_file_prefix(args)
+    savefile_prfx = get_savefile(args)
     print(savefile_prfx)

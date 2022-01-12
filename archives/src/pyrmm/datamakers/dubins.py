96758fbfae7cf4cc960cde6969eedb76ab8829a7
Ross Allen <ro27711@mit.edu>
2022-01-12 14:09:20 -0800

    migrating dubins datamaker to hydrazen

---
+++ archives/src/pyrmm/datamakers/dubins.py
@@ -0,0 +1,114 @@
+import argparse
+import hashlib
+import git
+import yaml
+from collections import OrderedDict
+from datetime import datetime
+from pathlib import Path
+
+from pyrmm.setups.dubins import DubinsPPMSetup
+
+HASH_LEN = 5
+DESCRIPTOR = 'dubins'
+SAVE_DIR = 'data'
+
+# Setup argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--speed', type=float, default=10.0, help='[m/s] speed of dubins vehicle')
+parser.add_argument('--min-turn-radius', type=float, default=50.0, help='[m] minimum turning radius of dubins vehicle')
+parser.add_argument('--obstacle-file', type=str, default='tests/border_640x400.ppm', help='path to ppm file representing obstacles')
+parser.add_argument('--policy', type=str, default='uniform_random', help='description of policy used for control propagation')
+parser.add_argument('--duration', type=float, default=2.0, help='[s] duration of policy propagation at each depth')
+parser.add_argument('--tree-depth', type=int, default=4, help='number of depth layers in propagation tree')
+parser.add_argument('--tree-branching', type=int, default=16, help='number of branches per depth in control propagation tree')
+parser.add_argument('--n-steps', type=int, default=2, help='number of intermediate steps in each control segment for collision checking')
+parser.add_argument('--n-samples', type=int, default=1024, help='number of top-level samples to draw for data generation')
+
+def get_savefile(inargs):
+    '''create the save filename using timestamps and hashes
+    
+    Returns:
+        save_fname : str
+            filename is formatted with the datetime when the file was called,
+            the git hash of the repo
+            string desriptor of the datatype
+            the sha1 hash of this files contents (may change between repo commits)
+            the sha1 hash of the input args
+            YYYYMMDD_HHMMSS_DESC_REPO_FILE_ARGS
+        configs : dict
+            configurations to be saved into yaml file
+    '''
+
+    configs = OrderedDict()
+    
+    # get source file and repo paths
+    src_fpath = str(Path(__file__).resolve())
+    repo = git.Repo(search_parent_directories=True)
+    repo_dir = repo.git_dir[:-len('.git')]
+    configs['src_file']= src_fpath[len(repo_dir):]
+
+    # get datetime for file naming
+    dt_str = datetime.now().strftime('%Y%m%d_%H%M%S')
+    configs['datetime'] = dt_str
+    # print(dt_str)
+
+    # get git repo hash for file naming
+    repo_hash = repo.head.object.hexsha
+    configs['git_hash'] = repo_hash
+
+    # hash file contents
+    with open (src_fpath, 'r') as thisfile:
+        file_str=thisfile.readlines()
+    file_str = ''.join([i for i in file_str])
+    file_hash = hashlib.sha1()
+    file_hash.update(file_str.encode())
+    configs['file_hash'] = file_hash.hexdigest()
+    
+    # hash input arguments
+    args_hash = hashlib.sha1()
+    args_hash.update(str(inargs).encode())
+    configs['args_hash'] = args_hash.hexdigest()
+
+    # create filename
+    save_fname = (
+        dt_str + '_' +
+        DESCRIPTOR + '_' +
+        repo_hash[:HASH_LEN] + '_' + 
+        file_hash.hexdigest()[:HASH_LEN] + '_' + 
+        args_hash.hexdigest()[:HASH_LEN])
+
+    # save configs as yaml
+    config_fname = Path(repo_dir).resolve().joinpath(SAVE_DIR, save_fname+'.yml')
+    with open(config_fname, 'w') as config_file:
+        # yaml.dump({**configs}, config_file)
+        for k, v in configs.items():
+            yaml.dump({k:v}, config_file)
+        yaml.dump(vars(args), config_file)
+
+    return save_fname, repo_dir
+
+
+if __name__ == "__main__":
+
+    # get input args
+    args = parser.parse_args()
+
+    # create data save files
+    savefile_prfx, repo_dir = get_savefile(args)
+    
+    # create dubins setup
+    obstacle_fpath = str(Path().joinpath(repo_dir, args.obstacle_file))
+    dubss = DubinsPPMSetup(
+        ppm_file=obstacle_fpath,
+        speed=args.speed,
+        min_turn_radius=args.min_turn_radius
+    )
+
+    # sample states to evaluate risk metrics
+    sampler = dubss.space_info.allocValidStateSampler()
+    ssamples = args.n_samples * [None] 
+    for i in range(args.n_samples):
+
+        # assign state
+        ssamples[i] = dubss.space_info.allocState()
+        sampler.sample(ssamples[i])
--- src/pyrmm/datamakers/dubins.py
+++ src/pyrmm/datamakers/dubins.py
@@ -1,60 +1,55 @@
-import argparse
+import hydra
+from hydra.core.config_store import ConfigStore
+from hydra_zen import builds, make_config, instantiate
+
 import hashlib
 import git
 import yaml
 from collections import OrderedDict
-from datetime import datetime
 from pathlib import Path
 
 from pyrmm.setups.dubins import DubinsPPMSetup
 
-HASH_LEN = 5
-DESCRIPTOR = 'dubins'
-SAVE_DIR = 'data'
+_HASH_LEN = 5
+_CONFIG_NAME = "dubins_datamaker_app"
 
 # Setup argument parser
-parser = argparse.ArgumentParser()
-parser.add_argument('--speed', type=float, default=10.0, help='[m/s] speed of dubins vehicle')
-parser.add_argument('--min-turn-radius', type=float, default=50.0, help='[m] minimum turning radius of dubins vehicle')
-parser.add_argument('--obstacle-file', type=str, default='tests/border_640x400.ppm', help='path to ppm file representing obstacles')
-parser.add_argument('--policy', type=str, default='uniform_random', help='description of policy used for control propagation')
-parser.add_argument('--duration', type=float, default=2.0, help='[s] duration of policy propagation at each depth')
-parser.add_argument('--tree-depth', type=int, default=4, help='number of depth layers in propagation tree')
-parser.add_argument('--tree-branching', type=int, default=16, help='number of branches per depth in control propagation tree')
-parser.add_argument('--n-steps', type=int, default=2, help='number of intermediate steps in each control segment for collision checking')
-parser.add_argument('--n-samples', type=int, default=1024, help='number of top-level samples to draw for data generation')
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--speed', type=float, default=10.0, help='[m/s] speed of dubins vehicle')
+# parser.add_argument('--min-turn-radius', type=float, default=50.0, help='[m] minimum turning radius of dubins vehicle')
+# parser.add_argument('--obstacle-file', type=str, default='tests/border_640x400.ppm', help='path to ppm file representing obstacles')
+# parser.add_argument('--policy', type=str, default='uniform_random', help='description of policy used for control propagation')
+# parser.add_argument('--duration', type=float, default=2.0, help='[s] duration of policy propagation at each depth')
+# parser.add_argument('--tree-depth', type=int, default=4, help='number of depth layers in propagation tree')
+# parser.add_argument('--tree-branching', type=int, default=16, help='number of branches per depth in control propagation tree')
+# parser.add_argument('--n-steps', type=int, default=2, help='number of intermediate steps in each control segment for collision checking')
+# parser.add_argument('--n-samples', type=int, default=1024, help='number of top-level samples to draw for data generation')
 
-def get_savefile(inargs):
+def get_file_info():
     '''create the save filename using timestamps and hashes
     
     Returns:
         save_fname : str
-            filename is formatted with the datetime when the file was called,
-            the git hash of the repo
-            string desriptor of the datatype
+            string desriptor of the datatype (name of source file)
+            the git hash of the repo state
             the sha1 hash of this files contents (may change between repo commits)
-            the sha1 hash of the input args
-            YYYYMMDD_HHMMSS_DESC_REPO_FILE_ARGS
-        configs : dict
-            configurations to be saved into yaml file
+            SRCFILE_REPOHASH_FILEHASH
+        repo_path : str
+            full path to repo head
     '''
 
-    configs = OrderedDict()
+    # configs = OrderedDict()
     
     # get source file and repo paths
     src_fpath = str(Path(__file__).resolve())
     repo = git.Repo(search_parent_directories=True)
-    repo_dir = repo.git_dir[:-len('.git')]
-    configs['src_file']= src_fpath[len(repo_dir):]
-
-    # get datetime for file naming
-    dt_str = datetime.now().strftime('%Y%m%d_%H%M%S')
-    configs['datetime'] = dt_str
-    # print(dt_str)
+    repo_path = repo.git_dir[:-len('.git')]
+    src_fname = str(Path(__file__).stem)
+    # configs['src_file']= src_fpath[len(repo_dir):]
 
     # get git repo hash for file naming
     repo_hash = repo.head.object.hexsha
-    configs['git_hash'] = repo_hash
+    # configs['git_hash'] = repo_hash
 
     # hash file contents
     with open (src_fpath, 'r') as thisfile:
@@ -62,53 +57,44 @@ def get_savefile(inargs):
     file_str = ''.join([i for i in file_str])
     file_hash = hashlib.sha1()
     file_hash.update(file_str.encode())
-    configs['file_hash'] = file_hash.hexdigest()
-    
-    # hash input arguments
-    args_hash = hashlib.sha1()
-    args_hash.update(str(inargs).encode())
-    configs['args_hash'] = args_hash.hexdigest()
+    # configs['file_hash'] = file_hash.hexdigest()
 
     # create filename
     save_fname = (
-        dt_str + '_' +
-        DESCRIPTOR + '_' +
-        repo_hash[:HASH_LEN] + '_' + 
-        file_hash.hexdigest()[:HASH_LEN] + '_' + 
-        args_hash.hexdigest()[:HASH_LEN])
+        src_fname + '_' +
+        repo_hash[:_HASH_LEN] + '_' + 
+        file_hash.hexdigest()[:_HASH_LEN])
 
-    # save configs as yaml
-    config_fname = Path(repo_dir).resolve().joinpath(SAVE_DIR, save_fname+'.yml')
-    with open(config_fname, 'w') as config_file:
-        # yaml.dump({**configs}, config_file)
-        for k, v in configs.items():
-            yaml.dump({k:v}, config_file)
-        yaml.dump(vars(args), config_file)
+    return save_fname, repo_path
 
-    return save_fname, repo_dir
+##############################################
+############# HYDARA-ZEN CONFIGS #############
+##############################################
 
+# DubinsPPMSetup Config
+_DEFAULT_SPEED = 10.0
+_DEFAULT_MIN_TURN_RADIUS = 50.0
+_DEFAULT_PPM_FILE = "tests/border_640x400.ppm"
+_, repo_path = get_file_info()
+DubinsPPMSetupConfig = builds(DubinsPPMSetup, populate_full_signature=True,
+    ppm_file = str(Path(repo_path).joinpath(_DEFAULT_PPM_FILE)),
+    speed=_DEFAULT_SPEED,
+    min_turn_radius=_DEFAULT_MIN_TURN_RADIUS
+)
+
+# Top-level configuration and store for command line interface
+Config = make_config(
+    dubins_ppm_setup=DubinsPPMSetupConfig
+)
+ConfigStore.instance().store(_CONFIG_NAME,Config)
+
+##############################################
+############### TASK FUNCTIONS ###############
+##############################################
+@hydra.main(config_path=None, config_name=_CONFIG_NAME)
+def task_function(cfg: Config):
+    '''Instantiate Dubins setup and generate risk metric data'''
+    obj = instantiate(cfg)
 
 if __name__ == "__main__":
-
-    # get input args
-    args = parser.parse_args()
-
-    # create data save files
-    savefile_prfx, repo_dir = get_savefile(args)
-    
-    # create dubins setup
-    obstacle_fpath = str(Path().joinpath(repo_dir, args.obstacle_file))
-    dubss = DubinsPPMSetup(
-        ppm_file=obstacle_fpath,
-        speed=args.speed,
-        min_turn_radius=args.min_turn_radius
-    )
-
-    # sample states to evaluate risk metrics
-    sampler = dubss.space_info.allocValidStateSampler()
-    ssamples = args.n_samples * [None] 
-    for i in range(args.n_samples):
-
-        # assign state
-        ssamples[i] = dubss.space_info.allocState()
-        sampler.sample(ssamples[i])
+    task_function()

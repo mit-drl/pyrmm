01877c8f4865d47e2193fa8f01fdc4cdc0f56ff5
Ross Allen <ro27711@mit.edu>
2022-01-13 12:16:23 -0800

    cleaning commented code

--- src/pyrmm/datagen/dubins.py
+++ src/pyrmm/datagen/dubins.py
@@ -17,18 +17,6 @@ from pyrmm.setups.dubins import DubinsPPMSetup
 _HASH_LEN = 5
 _CONFIG_NAME = "dubins_datamaker_app"
 
-# Setup argument parser
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--speed', type=float, default=10.0, help='[m/s] speed of dubins vehicle')
-# parser.add_argument('--min-turn-radius', type=float, default=50.0, help='[m] minimum turning radius of dubins vehicle')
-# parser.add_argument('--obstacle-file', type=str, default='tests/border_640x400.ppm', help='path to ppm file representing obstacles')
-# parser.add_argument('--policy', type=str, default='uniform_random', help='description of policy used for control propagation')
-# parser.add_argument('--duration', type=float, default=2.0, help='[s] duration of policy propagation at each depth')
-# parser.add_argument('--tree-depth', type=int, default=4, help='number of depth layers in propagation tree')
-# parser.add_argument('--tree-branching', type=int, default=16, help='number of branches per depth in control propagation tree')
-# parser.add_argument('--n-steps', type=int, default=2, help='number of intermediate steps in each control segment for collision checking')
-# parser.add_argument('--n-samples', type=int, default=1024, help='number of top-level samples to draw for data generation')
-
 def get_file_info():
     '''create the save filename using timestamps and hashes
     
@@ -112,19 +100,6 @@ _DEFAULT_N_BRANCHES = 16
 _DEFAULT_TREE_DEPTH = 4
 _DEFAULT_N_STEPS = 2
 _DEFAULT_POLICY = 'uniform_random'
-
-# def state_sampler_inputs_config(n_samples:int=_DEFAULT_N_SAMPLES):
-#     return {'n_samples': n_samples}
-# StateSamplerInputsConfig = pbuilds(state_sampler_inputs_config)
-
-# def risk_estimator_inputs(
-#     duration:float=_DEFAULT_DURATION, 
-#     n_branches:int=_DEFAULT_N_BRANCHES, 
-#     tree_depth:int=_DEFAULT_TREE_DEPTH, 
-#     n_steps:int=_DEFAULT_N_STEPS, 
-#     policy:str=_DEFAULT_POLICY):
-#     return {'duration': duration, 'n_branches': n_branches, 'tree_depth': tree_depth, 'n_steps': n_steps, 'policy': policy}
-# RiskEstimatorConfig = pbuilds(risk_estimator_inputs)
 
 # Top-level configuration and store for command line interface
 Config = make_config(

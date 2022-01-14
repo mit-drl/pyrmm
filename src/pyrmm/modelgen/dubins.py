eb6a416921e23afa1375eaee8c4879502dd36e86
Ross Allen <ro27711@mit.edu>
2022-01-14 12:23:59 -0800

    adding plotter

--- src/pyrmm/modelgen/dubins.py
+++ src/pyrmm/modelgen/dubins.py
@@ -1,6 +1,8 @@
 import yaml
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
 from pathlib import Path
 from typing import List
 from pytorch_lightning import LightningDataModule
@@ -30,10 +32,11 @@ class RiskMetricDataModule(LightningDataModule):
         RiskMetricDataModule.verify_hydrazen_rmm_data(dpaths)
 
         # load data objects
-        dobjs = len(dpaths) * [None]
+        data = []
         for i, dp in enumerate(dpaths):
-            dobjs[i] = torch.load(dp)
-            print(dobjs[i])
+            data.extend(torch.load(dp))
+
+        RiskMetricDataModule.plot_dubins_data(data, dpaths[0])
 
     @staticmethod
     def verify_hydrazen_rmm_data(datapaths: List):
@@ -58,6 +61,7 @@ class RiskMetricDataModule(LightningDataModule):
                 dur = cfg[U.DURATION]
                 depth = cfg[U.TREE_DEPTH]
                 policy = cfg[U.POLICY]
+                brnch = cfg[U.N_BRANCHES]
             
             else:
                 # check system parameters match
@@ -69,8 +73,47 @@ class RiskMetricDataModule(LightningDataModule):
                 assert np.isclose(cfg[U.DURATION], dur)
                 assert cfg[U.TREE_DEPTH] == depth
                 assert cfg[U.POLICY] == policy
+                assert cfg[U.N_BRANCHES] == brnch
             
-            
+    @staticmethod
+    def plot_dubins_data(data, datapath, cmap='coolwarm'):
+
+        cfg_path = datapath.parent.joinpath('.hydra','config.yaml')
+        with open(cfg_path, 'r') as cfg_file:
+            cfg = yaml.full_load(cfg_file)
+
+        assert cfg[U.SYSTEM_SETUP]['ppm_file'].split('/')[-1] == 'border_640x400.ppm' 
+
+        # unzip tuples of ssamples and rmetrics
+        ssamples, rmetrics = tuple(zip(*data))
+
+        # plot results
+        fig = plt.figure(1)
+        ax = fig.add_subplot(111)
+        # lbl = "{}-branches, {}-depth".format(brch, dpth)
+        xvals = [s.getX() for s in ssamples]
+        yvals = [s.getY() for s in ssamples]
+        uvals = [np.cos(s.getYaw()) for s in ssamples]
+        vvals = [np.sin(s.getYaw()) for s in ssamples]
+        ax.quiver(xvals, yvals, uvals, vvals, rmetrics, cmap=cmap)
+
+        # Draw in true risk metrics and obstacles
+        ax.axvline(x=0, label='Obstacle'.format(0.5), lw=4.0, c='k')
+        ax.axvline(x=640, label='Obstacle'.format(0.5), lw=4.0, c='k')
+        ax.axhline(y=0, label='Obstacle'.format(0.5), lw=4.0, c='k')
+        ax.axhline(y=400, label='Obstacle'.format(0.5), lw=4.0, c='k')
+        ax.set_title(
+            "Estimated Risk Metrics for Dubins Vehicle (speed={}, turn rad={})\n".format(cfg[U.SYSTEM_SETUP]['speed'], cfg[U.SYSTEM_SETUP]['min_turn_radius']) +
+            "in Constrained Box w/ uniform control sampling of duration={},\n".format(cfg[U.DURATION]) +
+            "tree depth={}, and branching factor={}".format(cfg[U.TREE_DEPTH], cfg[U.N_BRANCHES]) 
+        )
+        ax.set_xlim([0,640])
+        ax.set_ylim([0,400])
+        ax.set_xlabel("x-position")
+        ax.set_ylabel("y-position")
+        fig.colorbar(cm.ScalarMappable(None, cmap), ax=ax, label='failure probability')
+        fig.savefig('dubins_risk_estimation', bbox_inches='tight')
+        plt.show()
 
 ##############################################
 ############# HYDARA-ZEN CONFIGS #############
@@ -84,9 +127,12 @@ class RiskMetricDataModule(LightningDataModule):
 
 if __name__ == "__main__":
     rmm_data = RiskMetricDataModule([
-        'outputs/2022-01-14/10-36-03/datagen_dubins_739e6_ffce1.pt',
-        'outputs/2022-01-14/10-42-04/datagen_dubins_739e6_ffce1.pt',
-        'outputs/2022-01-14/10-47-03/datagen_dubins_739e6_ffce1.pt',
+        'outputs/2022-01-14/11-25-51/datagen_dubins_f8951_ebfd4.pt',
+        'outputs/2022-01-14/11-46-12/datagen_dubins_e6ecd_ebfd4.pt',
+        'outputs/2022-01-14/11-53-42/datagen_dubins_e6ecd_ebfd4.pt',
+        # 'outputs/2022-01-14/10-36-03/datagen_dubins_739e6_ffce1.pt',
+        # 'outputs/2022-01-14/10-42-04/datagen_dubins_739e6_ffce1.pt',
+        # 'outputs/2022-01-14/10-47-03/datagen_dubins_739e6_ffce1.pt',
         # './outputs/2022-01-13/15-23-10',
         # './outputs/2022-01-14/10-02-24',
         # './outputs/2022-01-14/10-09-51'

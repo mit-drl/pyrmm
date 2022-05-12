b05ba72c79cdc2c7ecd79083a705ed0a9494970b
Ross Allen <ro27711@mit.edu>
2022-05-12 14:55:32 -0700

    starting to generalize sample_risk_metrics

--- src/pyrmm/datagen/dubins.py
+++ src/pyrmm/datagen/dubins.py
@@ -1,10 +1,7 @@
 import hydra
-import torch
-import copyreg
 import multiprocess
 import time
 import numpy as np
-from functools import partial
 from pathlib import Path
 from hydra.core.config_store import ConfigStore
 from hydra_zen import make_config, instantiate, make_custom_builds_fn, builds
@@ -12,10 +9,10 @@ from hydra_zen import ZenField as zf
 
 import pyrmm.utils.utils as U
 from pyrmm.setups.dubins import DubinsPPMSetup
+from pyrmm.datagen.sampler import sample_risk_metrics
 
 _HASH_LEN = 5
 _CONFIG_NAME = "dubins_datagen_app"
-_MONITOR_RATE = 100
 
 ##############################################
 ################# UTILITIES ##################
@@ -40,17 +37,8 @@ def get_abs_path_str(rel_file_path):
 
 pbuilds = make_custom_builds_fn(populate_full_signature=True)
 
-# DubinsPPMSetup Config
-# _DEFAULT_PPM_FILE = "tests/border_640x400.ppm"
-# PPMFileConfig = pbuilds(get_abs_path_str, rel_file_path=_DEFAULT_PPM_FILE)
-
 _DEFAULT_SPEED = 10.0
 _DEFAULT_MIN_TURN_RADIUS = 50.0
-# DubinsPPMSetupConfig = pbuilds(DubinsPPMSetup,
-#     ppm_file = PPMFileConfig,
-#     speed=_DEFAULT_SPEED,
-#     min_turn_radius=_DEFAULT_MIN_TURN_RADIUS
-# )
 DubinsPPMSetupConfig = builds(DubinsPPMSetup,
     speed=_DEFAULT_SPEED,
     min_turn_radius=_DEFAULT_MIN_TURN_RADIUS,
@@ -92,70 +80,6 @@ ConfigStore.instance().store(_CONFIG_NAME,Config)
 ############### TASK FUNCTIONS ###############
 ##############################################
 
-def sample_risk_metrics(dubppm: DubinsPPMSetup, cfg_obj, save_name: str):
-    '''sample states of DubinsPPMSetup and compute risk metrics
-    
-    Args:
-        dubppm : DubinsPPMSetup
-            the dubins ppm setup object, with associated ppm of 
-            obstacle configuraton space, used for sampling and risk
-            metric computation
-        cfg_obj :
-            instantiate configuration object
-        save_name : str
-            name of pickle file to save risk metrics
-    '''
-
-    pool = multiprocess.Pool(getattr(cfg_obj, U.N_CORES), maxtasksperchild=cfg_obj.maxtasks)
-
-    # sample states to evaluate risk metrics
-    sampler = dubppm.space_info.allocStateSampler()
-    states = getattr(cfg_obj, U.N_SAMPLES) * [None] 
-    observations = getattr(cfg_obj, U.N_SAMPLES) * [None] 
-    for i in range(getattr(cfg_obj, U.N_SAMPLES)):
-
-        # assign state
-        states[i] = dubppm.space_info.allocState()
-        sampler.sampleUniform(states[i])
-
-        # get ray casts for sampled state
-        observations[i] = [dubppm.cast_ray(states[i], theta, cfg_obj.lidar.resolution) for theta in cfg_obj.lidar.angles] 
-
-        if i%_MONITOR_RATE ==  0:
-            print("State sampling and ray casting: completed {} of {}".format(i, len(states)))
-
-    # multiprocess implementation of parallel risk metric estimation
-    partial_estimateRiskMetric = partial(
-        dubppm.estimateRiskMetric, 
-        trajectory=None,
-        distance=getattr(cfg_obj, U.DURATION),
-        branch_fact=getattr(cfg_obj, U.N_BRANCHES),
-        depth=getattr(cfg_obj, U.TREE_DEPTH),
-        n_steps=getattr(cfg_obj, U.N_STEPS),
-        policy=getattr(cfg_obj, U.POLICY)
-    )
-
-    # use iterative map for process tracking
-    t_start = time.time()
-    rmetrics_iter = pool.imap(partial_estimateRiskMetric, states)
-
-    # track multiprocess progress
-    risk_metrics = []
-    for i,_ in enumerate(states):
-        risk_metrics.append(rmetrics_iter.next())
-        if i%_MONITOR_RATE ==  0:
-            print("Risk metric evaluation: completed {} of {} after {:.2f}".format(i, len(states), time.time()-t_start))
-
-    pool.close()
-    pool.join()
-
-    print("Subprocess elapsed time: {:.2f}".format(time.time()-t_start))
-
-    # save data for pytorch training
-    data = [i for i in zip(states, risk_metrics, observations)]
-    torch.save(data, open(save_name+".pt", "wb"))
-
-
 @hydra.main(config_path=None, config_name=_CONFIG_NAME)
 def task_function(cfg: Config):
     '''Instantiate Dubins setup and generate risk metric data'''
@@ -180,7 +104,7 @@ def task_function(cfg: Config):
 
         # sample states in ppm config and compute risk metrics
         print("\nRISK DATA GENERATION {} OF {}\nDUBINS VEHICLE IN OBSTACLE SPACE {}\n".format(i+1, len(ppm_paths), pp.name))
-        sample_risk_metrics(dubppm=dubins_ppm_setup, cfg_obj=obj, save_name=save_name)
+        sample_risk_metrics(sysset=dubins_ppm_setup, cfg_obj=obj, save_name=save_name)
         print("\n===================================")
 
     print("\nTotal elapsed time: {:.2f}".format(time.time()-t_start))
---
+++ src/pyrmm/datagen/sampler.py
@@ -0,0 +1,74 @@
+# tools to assist sampling of states and computing risk metrics in
+# data generation process
+
+import torch
+import time
+import multiprocess
+from functools import partial
+
+import pyrmm.utils.utils as U
+from pyrmm.setups import SystemSetup
+
+_MONITOR_RATE = 100
+
+def sample_risk_metrics(sysset: SystemSetup, cfg_obj, save_name: str):
+    '''sample states of DubinsPPMSetup and compute risk metrics
+    
+    Args:
+        sysset : SystemSetup
+            the vehicle-config space setup object to be used
+            for sampling and risk metric computation
+        cfg_obj :
+            instantiate configuration object
+        save_name : str
+            name of pickle file to save risk metrics
+    '''
+
+    pool = multiprocess.Pool(getattr(cfg_obj, U.N_CORES), maxtasksperchild=cfg_obj.maxtasks)
+
+    # sample states to evaluate risk metrics
+    sampler = sysset.space_info.allocStateSampler()
+    states = getattr(cfg_obj, U.N_SAMPLES) * [None] 
+    observations = getattr(cfg_obj, U.N_SAMPLES) * [None] 
+    for i in range(getattr(cfg_obj, U.N_SAMPLES)):
+
+        # assign state
+        states[i] = sysset.space_info.allocState()
+        sampler.sampleUniform(states[i])
+
+        # get ray casts for sampled state
+        observations[i] = [sysset.cast_ray(states[i], theta, cfg_obj.lidar.resolution) for theta in cfg_obj.lidar.angles] 
+
+        if i%_MONITOR_RATE ==  0:
+            print("State sampling and ray casting: completed {} of {}".format(i, len(states)))
+
+    # multiprocess implementation of parallel risk metric estimation
+    partial_estimateRiskMetric = partial(
+        sysset.estimateRiskMetric, 
+        trajectory=None,
+        distance=getattr(cfg_obj, U.DURATION),
+        branch_fact=getattr(cfg_obj, U.N_BRANCHES),
+        depth=getattr(cfg_obj, U.TREE_DEPTH),
+        n_steps=getattr(cfg_obj, U.N_STEPS),
+        policy=getattr(cfg_obj, U.POLICY)
+    )
+
+    # use iterative map for process tracking
+    t_start = time.time()
+    rmetrics_iter = pool.imap(partial_estimateRiskMetric, states)
+
+    # track multiprocess progress
+    risk_metrics = []
+    for i,_ in enumerate(states):
+        risk_metrics.append(rmetrics_iter.next())
+        if i%_MONITOR_RATE ==  0:
+            print("Risk metric evaluation: completed {} of {} after {:.2f}".format(i, len(states), time.time()-t_start))
+
+    pool.close()
+    pool.join()
+
+    print("Subprocess elapsed time: {:.2f}".format(time.time()-t_start))
+
+    # save data for pytorch training
+    data = [i for i in zip(states, risk_metrics, observations)]
+    torch.save(data, open(save_name+".pt", "wb"))

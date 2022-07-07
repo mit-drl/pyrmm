7fbc638b2ab31589fbbe811500f80ae04b5e702d
Ross Allen <ro27711@mit.edu>
2022-07-07 07:33:01 -0700

    simplifying sc bug

--- notebooks/sc_bug_script.py
+++ notebooks/sc_bug_script.py
@@ -1,5 +1,6 @@
-from pyrmm.setups.quadrotor import QuadrotorPyBulletSetup
-quadpb_setup = QuadrotorPyBulletSetup()
-cspace = quadpb_setup.space_info.getControlSpace()
+from ompl import base as ob
+from ompl import control as oc
+sspace = ob.RealVectorStateSpace(1)
+cspace = oc.RealVectorControlSpace(sspace, 1)
 c = cspace.allocControl()
-c[0] = 0.0
+c[0] = 1.0

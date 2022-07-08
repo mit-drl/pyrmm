0d61f8fce261f36e99d538b06ed527098c623b26
Ross Allen <ro27711@mit.edu>
2022-07-08 14:16:51 -0700

    versioning package

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup, find_packages
 
+def get_version():
+    path = "src/pyrmm/version.py"
+    with open(path) as file:
+        lines = file.readlines()
+
+    for line in lines:
+        if line.startswith("VERSION"):
+            return line.strip().split('=')[-1].strip().strip('"')
+    raise RuntimeError("bad version data in version.py")
+
 setup(name="pyrmm",
+    version=get_version(),
     packages=find_packages('src'),
     package_dir={'': 'src'},
     python_requires=">=3",
